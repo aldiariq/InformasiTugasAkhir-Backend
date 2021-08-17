c948f8c744de007b5ccf34f223a8f0fcdf3f9c9f
aldiariq <aldi48767@gmail.com>
2021-08-17 11:19:00 +0700

    dockerize project

--- config.py
+++ config.py
@@ -3,6 +3,10 @@ import os
 basedir = os.path.abspath(os.path.dirname(__file__))
 
 class Config(object):
+    # DATABASE_HOST = '127.0.0.1:3306'
+    # DATABASE_USERNAME = 'root'
+    # DATABASE_PASSWORD = ''
+    # DATABASE_NAME = 'informasitugasakhir'
     DATABASE_HOST = str(os.environ.get("DATABASE_HOST"))
     DATABASE_USERNAME = str(os.environ.get("DATABASE_USERNAME"))
     DATABASE_PASSWORD = str(os.environ.get("DATABASE_PASSWORD"))
@@ -12,8 +16,10 @@ class Config(object):
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     SQLALCHEMY_RECORD_QUERIES = True
     JSON_SORT_KEYS = False
+    # JWT_SECRET = 'BelajarFlaskMysql'
     JWT_SECRET = str(os.environ.get("JWT_SECRET"))
     JWT_SECRET_KEY = "HS256"
 
+    # UPLOAD_FOLDER = str('upload')
     UPLOAD_FOLDER = str(os.environ.get("UPLOAD_FOLDER"))
     MAX_UPLOAD_SIZE = 3 * 1024 * 1024
